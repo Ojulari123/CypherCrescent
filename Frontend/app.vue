08f9d8a9a099be7e74c43609e188426838fe3730
OJ <adeoluwaojulari@gmail.com>
2026-06-19 04:13:01 -0400

    FOUC fix yet again

--- Frontend/app.vue
+++ Frontend/app.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-const ui = useUiStore()
-// Sync Pinia dark state with whatever the inline head script already applied.
-onMounted(() => ui.initTheme())
-</script>
-
 <template>
   <div>
     <NuxtLayout>
